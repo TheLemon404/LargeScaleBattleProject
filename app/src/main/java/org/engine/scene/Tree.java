622bfbcf4710a06ea213af374236750267bf6350
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2026-06-21 14:45:47 -0500

    supplier

--- app/src/main/java/org/engine/scene/Tree.java
+++ app/src/main/java/org/engine/scene/Tree.java
@@ -1,9 +1,14 @@
 package org.engine.scene;
 
-import java.util.*;
+import java.util.function.Supplier;
 
 public class Tree {
 
     public static Entity root = new Entity();
     public static EntityRegistry registry = new EntityRegistry();
+
+    public static <T extends Entity> T createEntity(Supplier<T> supplier) {
+        EntityFactory<T> factory = new EntityFactory<T>(supplier);
+        return factory.createInstance();
+    }
 }
